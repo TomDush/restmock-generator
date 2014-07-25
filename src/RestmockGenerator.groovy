75f8e3e02bcf2f253e004dcfbdef693879e6d07c
Thomas Duchatelle <duchatelle.thomas@gmail.com>
2014-07-25 18:31:29 +0100

    Again a test...

--- src/RestmockGenerator.groovy
+++ src/RestmockGenerator.groovy
@@ -1,2 +1,18 @@
+import groovyx.net.http.RESTClient;
+import groovyx.net.http.HttpResponseDecorator;
+import org.apache.http.HttpRequest;
+import org.apache.http.protocol.HttpContext;
+import org.apache.http.HttpRequestInterceptor;
+import groovy.json.JsonSlurper;
+import static groovyx.net.http.Method.*;
+import static groovyx.net.http.ContentType.*;
+
+@Grab(value = 'org.codehaus.groovy:groovy-all:1.7.5', initClass = false)
+@Grapes([
+        @Grab(group = 'org.codehaus.groovy.modules.http-builder',
+                module = 'http-builder', version = '0.5.1'),
+        @GrabExclude('org.codehaus.groovy:groovy')
+])
+def greeting = "hello world"
 
 println "Hello World! I'm a groovy script!"
