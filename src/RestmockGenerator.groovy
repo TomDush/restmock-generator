e1df41b37d8529efdb5b7522f367d850508b3b84
Thomas Duchatelle <duchatelle.thomas@gmail.com>
2014-07-26 18:59:11 +0100

    Can do request on REST server :)

--- src/RestmockGenerator.groovy
+++ src/RestmockGenerator.groovy
@@ -1,18 +1,25 @@
-import groovyx.net.http.RESTClient;
-import groovyx.net.http.HttpResponseDecorator;
-import org.apache.http.HttpRequest;
-import org.apache.http.protocol.HttpContext;
-import org.apache.http.HttpRequestInterceptor;
-import groovy.json.JsonSlurper;
-import static groovyx.net.http.Method.*;
-import static groovyx.net.http.ContentType.*;
-
-@Grab(value = 'org.codehaus.groovy:groovy-all:1.7.5', initClass = false)
+@Grab(value = 'org.codehaus.groovy:groovy-all:1.7.5')
+@Grab(group = 'org.codehaus.groovy.modules.http-builder', module = 'http-builder', version = '0.5.1')
 @Grapes([
-        @Grab(group = 'org.codehaus.groovy.modules.http-builder',
-                module = 'http-builder', version = '0.5.1'),
         @GrabExclude('org.codehaus.groovy:groovy')
 ])
-def greeting = "hello world"
+import groovyx.net.http.RESTClient
 
-println "Hello World! I'm a groovy script!"
+
+// How to make Intellij resolving Grab (groovy) imports?
+
+def greeting = "world"
+println "Hello ${greeting} !"
+
+def medima = new RESTClient('http://dush-temp:8080/api/')
+
+println 'Get random list...'
+def resp = medima.get(
+        path: 'movies/random.json',
+        query: [notNullFields: 'POSTER', size: 20]
+)
+
+List movies = resp.data.elements
+println "${movies.size()} random movies: "
+movies.each { println "\t- ${it.title}" }
+println ""
